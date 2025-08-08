24babfe0c8b2471984e3b9181cd523c49c9e4fff
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-08-08 15:02:21 +0100

    from mac

---
+++ main_run.sh
@@ -0,0 +1,6 @@
+python experiments/evaluate_unified_editing.py \
+--alg_name=EMMET \
+--num_edits=4 \
+--model_name=meta-llama/Meta-Llama-3-8B-Instruct \
+--hparams_fname=gpt2-xl.json \
+--ds_name=cf
