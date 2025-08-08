b41d480b680efcf8f0442a72e89034306d660607
byan19 <12835163+aisdante@user.noreply.gitee.com>
2025-08-08 15:04:27 +0100

    from mac

--- main_run.sh
+++ main_run.sh
@@ -1,6 +1,6 @@
 python experiments/evaluate_unified_editing.py \
 --alg_name=EMMET \
 --num_edits=4 \
---model_name=meta-llama/Llama-2-7b-hf \
+--model_name=Llama-2-7b \
 --hparams_fname=Llama-2-7b \
 --ds_name=cf
